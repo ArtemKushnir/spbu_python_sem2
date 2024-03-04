afb05721c284bab4e952c977ef0b2a5f76497003
Artem Kushnir <artem.kushnir.79@gmail.com>
2024-03-04 23:02:41 +0300

    fix types and imports

--- src/homeworks/homework1/registry.py
+++ src/homeworks/homework1/registry.py
@@ -1,25 +1,25 @@
-from typing import Generic, TypeVar
+from typing import Callable, Generic, Optional, TypeVar
 
 INTERFACE = TypeVar("INTERFACE")
 
 
 class Registry(Generic[INTERFACE]):
-    def __init__(self, default=None):
-        self._default = default
-        self._registry_storage = {}
+    def __init__(self, default: Optional[INTERFACE] = None) -> None:
+        self._default: Optional[INTERFACE] = default
+        self._registry_storage: dict[str, INTERFACE] = {}
 
-    def register(self, name):
+    def register(self, name: str) -> Callable:
         print(name in self._registry_storage, self._registry_storage)
         if name in self._registry_storage:
             raise ValueError("This name is already reserved")
 
-        def _decorator(cls):
+        def _decorator(cls: INTERFACE) -> INTERFACE:
             self._registry_storage[name] = cls
             return cls
 
         return _decorator
 
-    def dispatch(self, name):
+    def dispatch(self, name: str) -> Optional[INTERFACE]:
         if name in self._registry_storage or self._default:
             return self._registry_storage.get(name, self._default)
         raise ValueError("No class with this name found")
--- tests/homeworks/homework1/test_registry.py
+++ tests/homeworks/homework1/test_registry.py
@@ -1,7 +1,8 @@
-import pytest
-from src.homeworks.homework1 import registry
-from typing import Mapping
+from typing import Mapping, Optional
 
+import pytest
+
+from src.homeworks.homework1 import registry
 
 MAPPING_REGISTRY1 = registry.Registry[Mapping](default=dict)
 MAPPING_REGISTRY2 = registry.Registry[Mapping]()
@@ -9,36 +10,25 @@ MAPPING_REGISTRY2 = registry.Registry[Mapping]()
 
 @MAPPING_REGISTRY1.register("bst")
 class BST(Mapping):
-    def __getitem__(self, item):
+    def __getitem__(self, item) -> None:
         pass
 
-    def __len__(self):
+    def __len__(self) -> None:
         pass
 
-    def __iter__(self):
+    def __iter__(self) -> None:
         pass
 
 
 @MAPPING_REGISTRY2.register("avl_tree")
 class AVLTree(Mapping):
-    def __getitem__(self, item):
+    def __getitem__(self, item) -> None:
         pass
 
-    def __len__(self):
+    def __len__(self) -> None:
         pass
 
-    def __iter__(self):
-        pass
-
-
-class Cartesian_tree(Mapping):
-    def __getitem__(self, item):
-        pass
-
-    def __len__(self):
-        pass
-
-    def __iter__(self):
+    def __iter__(self) -> None:
         pass
 
 
@@ -49,7 +39,7 @@ class Cartesian_tree(Mapping):
         (MAPPING_REGISTRY2, "avl_tree", AVLTree),
     ],
 )
-def test_register(register, name, expected):
+def test_register(register, name, expected) -> None:
     assert name in register._registry_storage
     assert issubclass(register._registry_storage[name], expected)
 
@@ -58,26 +48,26 @@ def test_register(register, name, expected):
     "register,name,expected",
     [(MAPPING_REGISTRY1, "bst", BST), (MAPPING_REGISTRY1, "test1", dict), (MAPPING_REGISTRY2, "avl_tree", AVLTree)],
 )
-def test_dispatch(register, name, expected):
+def test_dispatch(register, name, expected) -> None:
     actual = register.dispatch(name)()
     assert isinstance(actual, expected)
 
 
-def test_raise_exception_register():
+def test_raise_exception_register() -> None:
     with pytest.raises(ValueError):
 
         @MAPPING_REGISTRY1.register("bst")
         class Test(Mapping):
-            def __getitem__(self, item):
+            def __getitem__(self, item) -> None:
                 pass
 
-            def __len__(self):
+            def __len__(self) -> Optional[int]:
                 pass
 
-            def __iter__(self):
+            def __iter__(self) -> None:
                 pass
 
 
-def test_raise_exception_dispatch():
+def test_raise_exception_dispatch() -> None:
     with pytest.raises(ValueError):
         MAPPING_REGISTRY2.dispatch("test2")
