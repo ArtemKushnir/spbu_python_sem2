e3405ded5d75eac8d1e8d6989c85f0c7b8a29540
Artem Kushnir <artem.kushnir.79@gmail.com>
2024-03-22 19:42:27 +0300

    fix generic type, refactoring tests

--- src/homeworks/homework1/registry.py
+++ src/homeworks/homework1/registry.py
@@ -1,25 +1,53 @@
-from typing import Callable, Generic, Optional, TypeVar
+from collections import Counter, OrderedDict, UserDict, defaultdict
+from typing import Callable, Generic, Optional, Type, TypeVar
 
-INTERFACE = TypeVar("INTERFACE")
+R = TypeVar("R")
 
 
-class Registry(Generic[INTERFACE]):
-    def __init__(self, default: Optional[INTERFACE] = None) -> None:
-        self._default: Optional[INTERFACE] = default
-        self._registry_storage: dict[str, INTERFACE] = {}
+class Registry(Generic[R]):
+    def __init__(self, default: Optional[Type[R]] = None) -> None:
+        self._default: Optional[Type[R]] = default
+        self._registry_storage: dict[str, Type[R]] = {}
 
     def register(self, name: str) -> Callable:
-        print(name in self._registry_storage, self._registry_storage)
         if name in self._registry_storage:
             raise ValueError("This name is already reserved")
 
-        def _decorator(cls: INTERFACE) -> INTERFACE:
+        def _decorator(cls: Type[R]) -> Type[R]:
             self._registry_storage[name] = cls
             return cls
 
         return _decorator
 
-    def dispatch(self, name: str) -> Optional[INTERFACE]:
-        if name in self._registry_storage or self._default:
-            return self._registry_storage.get(name, self._default)
+    def dispatch(self, name: str) -> Type[R]:
+        dispatch_class = self._registry_storage.get(name, self._default)
+        if dispatch_class is not None:
+            return dispatch_class
         raise ValueError("No class with this name found")
+
+
+def main() -> None:
+    mapping_registry = Registry[dict](dict)
+    mapping_registry.register("default_dict")(defaultdict)
+    mapping_registry.register("ordered_dict")(OrderedDict)
+    mapping_registry.register("user_dict")(UserDict)
+    mapping_registry.register("counter")(Counter)
+    print("select one of the following dictionaries:\n1.DefaultDict\n2.OrderedDict\n3.UserDict\n4.Counter")
+    user_input = input()
+    if user_input == "1":
+        user_dict = mapping_registry.dispatch("default_dict")()
+    elif user_input == "2":
+        user_dict = mapping_registry.dispatch("ordered_dict")()
+    elif user_input == "3":
+        user_dict = mapping_registry.dispatch("user_dict")()
+    elif user_input == "4":
+        user_dict = mapping_registry.dispatch("counter")()
+    user_dict[1] = "Python"
+    user_dict[2] = "C"
+    user_dict[3] = "Java"
+    user_dict[4] = "C#"
+    print("Your dict with random data:", user_dict)
+
+
+if __name__ == "__main__":
+    main()
--- tests/homeworks/homework1/test_registry.py
+++ tests/homeworks/homework1/test_registry.py
@@ -1,73 +1,80 @@
-from typing import Mapping, Optional
+from typing import Mapping
 
 import pytest
 
 from src.homeworks.homework1 import registry
 
-MAPPING_REGISTRY1 = registry.Registry[Mapping](default=dict)
-MAPPING_REGISTRY2 = registry.Registry[Mapping]()
 
-
-@MAPPING_REGISTRY1.register("bst")
 class BST(Mapping):
-    def __getitem__(self, item) -> None:
-        pass
-
-    def __len__(self) -> None:
-        pass
-
-    def __iter__(self) -> None:
-        pass
+    pass
 
 
-@MAPPING_REGISTRY2.register("avl_tree")
 class AVLTree(Mapping):
-    def __getitem__(self, item) -> None:
-        pass
-
-    def __len__(self) -> None:
-        pass
-
-    def __iter__(self) -> None:
-        pass
+    pass
 
 
-@pytest.mark.parametrize(
-    "register,name,expected",
-    [
-        (MAPPING_REGISTRY1, "bst", BST),
-        (MAPPING_REGISTRY2, "avl_tree", AVLTree),
-    ],
-)
-def test_register(register, name, expected) -> None:
-    assert name in register._registry_storage
-    assert issubclass(register._registry_storage[name], expected)
+class Treap(Mapping):
+    pass
 
 
-@pytest.mark.parametrize(
-    "register,name,expected",
-    [(MAPPING_REGISTRY1, "bst", BST), (MAPPING_REGISTRY1, "test1", dict), (MAPPING_REGISTRY2, "avl_tree", AVLTree)],
-)
-def test_dispatch(register, name, expected) -> None:
-    actual = register.dispatch(name)()
-    assert isinstance(actual, expected)
+class TestRegistryWithDefault:
+    MAPPING_REGISTRY1 = registry.Registry[Mapping](default=dict)
+    MAPPING_REGISTRY1.register("bst")(BST)
+    MAPPING_REGISTRY1.register("avl_tree")(AVLTree)
+    MAPPING_REGISTRY1.register("treap")(Treap)
+
+    @pytest.mark.parametrize(
+        "name,expected",
+        [("bst", BST), ("avl_tree", AVLTree), ("treap", Treap)],
+    )
+    def test_register(self, name, expected):
+        assert name in self.MAPPING_REGISTRY1._registry_storage
+        assert issubclass(self.MAPPING_REGISTRY1._registry_storage[name], expected)
+
+    @pytest.mark.parametrize(
+        "name,expected",
+        [("bst", BST), ("avl_tree", AVLTree), ("treap", Treap)],
+    )
+    def test_dispatch(self, name, expected):
+        actual = self.MAPPING_REGISTRY1.dispatch(name)
+        assert issubclass(actual, expected)
+
+    @pytest.mark.parametrize("name,expected", [("test1", dict), ("qwerty", dict), ("python", dict)])
+    def test_dispatch_return_default(self, name, expected):
+        actual = self.MAPPING_REGISTRY1.dispatch(name)
+        assert issubclass(actual, expected)
+
+    def test_raise_exception_register(self):
+        with pytest.raises(ValueError):
+            self.MAPPING_REGISTRY1.register("bst")
 
 
-def test_raise_exception_register() -> None:
-    with pytest.raises(ValueError):
+class TestRegistry:
+    MAPPING_REGISTRY2 = registry.Registry[Mapping]()
+    MAPPING_REGISTRY2.register("bst")(BST)
+    MAPPING_REGISTRY2.register("avl")(AVLTree)
+    MAPPING_REGISTRY2.register("cartesian_tree")(Treap)
 
-        @MAPPING_REGISTRY1.register("bst")
-        class Test(Mapping):
-            def __getitem__(self, item) -> None:
-                pass
+    @pytest.mark.parametrize(
+        "name,expected",
+        [("bst", BST), ("avl", AVLTree), ("cartesian_tree", Treap)],
+    )
+    def test_register(self, name, expected):
+        assert name in self.MAPPING_REGISTRY2._registry_storage
+        assert issubclass(self.MAPPING_REGISTRY2._registry_storage[name], expected)
 
-            def __len__(self) -> Optional[int]:
-                pass
+    @pytest.mark.parametrize(
+        "name,expected",
+        [("bst", BST), ("cartesian_tree", Treap), ("avl", AVLTree)],
+    )
+    def test_dispatch(self, name, expected):
+        actual = self.MAPPING_REGISTRY2.dispatch(name)
+        assert issubclass(actual, expected)
 
-            def __iter__(self) -> None:
-                pass
+    def test_raise_exception_dispatch(self):
+        with pytest.raises(ValueError):
+            self.MAPPING_REGISTRY2.dispatch("test2")
 
-
-def test_raise_exception_dispatch() -> None:
-    with pytest.raises(ValueError):
-        MAPPING_REGISTRY2.dispatch("test2")
+    def test_raise_exception_register(self):
+        with pytest.raises(ValueError):
+            self.MAPPING_REGISTRY2.register("avl")
