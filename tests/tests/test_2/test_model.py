bb7f5ce53c7d1cc631617ef246688a0ac8e2ea3c
Artem Kushnir <artem.kushnir.79@gmail.com>
2024-05-03 00:00:30 +0300

    Test#2.Task1.KushnirArtem

---
+++ tests/tests/test_2/test_model.py
@@ -0,0 +1,43 @@
+import asyncio
+
+import pytest
+import requests
+
+from src.tests.test_2.model import QuotesModel
+
+
+class TestModel:
+    model = QuotesModel()
+
+    @pytest.mark.parametrize("quote_count", [5, 15, 25])
+    def test_parse_last(self, quote_count):
+        quotes = asyncio.run(self.model.parse(quote_count, ""))
+        assert len(quotes) == quote_count
+
+    @pytest.mark.parametrize("quote_count", [1, 2, 5])
+    def test_parse_best(self, quote_count):
+        quotes = asyncio.run(self.model.parse(quote_count, "best"))
+        assert len(quotes) == quote_count
+
+    @pytest.mark.parametrize("quote_count", [5, 15, 25])
+    def test_parse_random(self, quote_count):
+        quotes = asyncio.run(self.model.parse(quote_count, "random"))
+        assert len(quotes) == quote_count
+
+    @pytest.mark.parametrize(
+        "url",
+        [
+            "https://xn--80abh7bk0c.xn--p1ai/",
+            "https://xn--80abh7bk0c.xn--p1ai/best",
+            "https://xn--80abh7bk0c.xn--p1ai/byrating",
+        ],
+    )
+    def test_get_text_page(self, url):
+        text = self.model.get_text_page(url)
+        expected = requests.get(url).text
+        assert text == expected
+
+    @pytest.mark.parametrize("url", ["demo", "test", "fall"])
+    def test_raise_exception(self, url):
+        with pytest.raises(ConnectionError):
+            self.model.get_text_page(url)
