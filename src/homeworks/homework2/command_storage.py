3a6f51274349e14ae3863aa8c214a568184dd2b8
ArtemKushnir <144917939+ArtemKushnir@users.noreply.github.com>
2024-05-27 12:34:46 +0300

    Homework2.Task1.Kushnir (#4)
    * Homework1.Task1.Kushnir
    
    * black)
    
    * fix types and imports
    
    * fix generic type, refactoring tests
    
    * change default class in example, fix return type in register
    
    * moved name check inside the decorator for the register to work correctly
    
    * Homework2.Task1.Kushnir
    
    * fix imports
    
    * add .hypothesis to gitignor
    
    * add init file to src
    
    * decomposed module
    
    * add library to requirements
    
    * fix "Add" action and typing in "CommandStorage"
    
    * fix isort error
    
    * fix: replace type check inside action, fix: add action
    
    * fix: pytest, fix: 'CollectionError'
    
    * replace check type collection in abstract class
    
    * fix type anotation, add new actions
    
    * add check type in undo_action, change get_info function
    
    * add abcMeta to actions

---
+++ src/homeworks/homework2/command_storage.py
@@ -0,0 +1,18 @@
+from src.homeworks.homework2.actions import *
+
+
+class PerformedCommandStorage:
+    def __init__(self, numbers_collection: MutableSequence[int]):
+        self.actions: list[Action] = []
+        self.numbers: MutableSequence[int] = numbers_collection
+
+    def apply(self, action: Action) -> None:
+        action.do_action(self.numbers)
+        self.actions.append(action)
+
+    def cancel(self) -> None:
+        if len(self.actions) > 0:
+            self.actions[-1].undo_action(self.numbers)
+            self.actions.pop()
+        else:
+            raise ActionIndexError("No action was performed")
